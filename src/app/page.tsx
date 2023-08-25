316e238868019f5687de6a472b7748b909e2cbbb
rohith purella <rohith.purella@bighaat.com>
2023-08-25 15:24:00 +0530

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,99 +49,101 @@ export default function Home() {
             </div>
           </section>
         </section>
-        <section className={`${styles['home-about-section']} mx-auto px-6`}>
-          <div className={`flex items-center justify-center`}>
-            <div className={`${styles['home-about-description']} ${styles['description_container']}`}>
-              <h1 style={{ fontSize: "2.6em" }}>
-                LET ME <span className={styles['purple']}> INTRODUCE </span> MYSELF
-              </h1>
-              <p className={styles['home-about-body']}>
-                I fell in love with programming and I have atleast learnt
-                something, I think… 🤷‍♂️
-                <br />
-                <br />I am fluent in
-                {/* classics like */}
-                <i>
-                  <b className={styles['purple']}> Javascript. </b>
-                </i>
-                <br />
-                <br />
-                {`My field of Interest's are building new `} &nbsp;
-                <i>
-                  <b className={styles['purple']}>Web Technologies and Products </b> and
-                  also in areas related to{" "}
-                  <b className={styles['purple']}>
-                    E-Commerce.
-                  </b>
-                </i>
-                <br />
-                <br />
-                Whenever possible, I also apply my passion for developing products
-                with <b className={styles['purple']}>Node.js</b> and
-                <i>
-                  <b className={styles['purple']}>
-                    {" "}
-                    Modern Javascript Library and Frameworks
-                  </b>
-                </i>
-                &nbsp; like
-                <i>
-                  <b className={styles['purple']}> React.js , Next.js and React-Native</b>
-                </i>
-              </p>
+        <div>
+          <section className={`${styles['home-about-section']} mx-auto px-6`}>
+            <div className={`flex items-center justify-center`}>
+              <div className={`${styles['home-about-description']} ${styles['description_container']}`}>
+                <h1 style={{ fontSize: "2.6em" }}>
+                  LET ME <span className={styles['purple']}> INTRODUCE </span> MYSELF
+                </h1>
+                <p className={styles['home-about-body']}>
+                  I fell in love with programming and I have atleast learnt
+                  something, I think… 🤷‍♂️
+                  <br />
+                  <br />I am fluent in
+                  {/* classics like */}
+                  <i>
+                    <b className={styles['purple']}> Javascript. </b>
+                  </i>
+                  <br />
+                  <br />
+                  {`My field of Interest's are building new `} &nbsp;
+                  <i>
+                    <b className={styles['purple']}>Web Technologies and Products </b> and
+                    also in areas related to{" "}
+                    <b className={styles['purple']}>
+                      E-Commerce.
+                    </b>
+                  </i>
+                  <br />
+                  <br />
+                  Whenever possible, I also apply my passion for developing products
+                  with <b className={styles['purple']}>Node.js</b> and
+                  <i>
+                    <b className={styles['purple']}>
+                      {" "}
+                      Modern Javascript Library and Frameworks
+                    </b>
+                  </i>
+                  &nbsp; like
+                  <i>
+                    <b className={styles['purple']}> React.js , Next.js and React-Native</b>
+                  </i>
+                </p>
+              </div>
+              <div className={styles['myAvtar']}>
+                <Image
+                  width={240}
+                  height={240}
+                  src={'/assets/svgs/avatar.svg'}
+                  alt="home pic"
+                  className={styles['img-fluid']}
+                  style={{ maxHeight: "240px" }}
+                />
+              </div>
             </div>
-            <div className={styles['myAvtar']}>
-              <Image
-                width={240}
-                height={240}
-                src={'/assets/svgs/avatar.svg'}
-                alt="home pic"
-                className={styles['img-fluid']}
-                style={{ maxHeight: "240px" }}
-              />
+            <div>
             </div>
-          </div>
-          <div>
-          </div>
-        </section>
-        <section className={`${styles['home-about-social']} pb-4`}>
-          <h1>FIND ME ON</h1>
-          <p>
-            Feel free to <span className={styles['purple']}>connect </span>with me
-          </p>
-          <ul className={styles['home-about-social-links']}>
-            <li className={styles["social-icons"]}>
-              <Link
-                href="https://github.com/purellarohith"
-                target="_blank"
-                rel="noreferrer"
-                className={`${styles['icon-colour']}  ${styles['home-social-icons']}`}
-              >
-                <AiFillGithub />
-              </Link>
-            </li>
-            <li className={styles["social-icons"]}>
-              <Link
-                href="https://twitter.com/iamrohithpatel"
-                target="_blank"
-                rel="noreferrer"
-                className={`${styles['icon-colour']}  ${styles['home-social-icons']}`}
-              >
-                <AiOutlineTwitter />
-              </Link>
-            </li>
-            <li className={styles["social-icons"]}>
-              <Link
-                href="https://www.linkedin.com/in/rohith-purella-b66066211/"
-                target="_blank"
-                rel="noreferrer"
-                className={`${styles['icon-colour']}  ${styles['home-social-icons']}`}
-              >
-                <FaLinkedinIn />
-              </Link>
-            </li>
-          </ul>
-        </section>
+          </section>
+          <section className={`${styles['home-about-social']} pb-4`}>
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className={styles['purple']}>connect </span>with me
+            </p>
+            <ul className={styles['home-about-social-links']}>
+              <li className={styles["social-icons"]}>
+                <Link
+                  href="https://github.com/purellarohith"
+                  target="_blank"
+                  rel="noreferrer"
+                  className={`${styles['icon-colour']}  ${styles['home-social-icons']}`}
+                >
+                  <AiFillGithub />
+                </Link>
+              </li>
+              <li className={styles["social-icons"]}>
+                <Link
+                  href="https://twitter.com/iamrohithpatel"
+                  target="_blank"
+                  rel="noreferrer"
+                  className={`${styles['icon-colour']}  ${styles['home-social-icons']}`}
+                >
+                  <AiOutlineTwitter />
+                </Link>
+              </li>
+              <li className={styles["social-icons"]}>
+                <Link
+                  href="https://www.linkedin.com/in/rohith-purella-b66066211/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className={`${styles['icon-colour']}  ${styles['home-social-icons']}`}
+                >
+                  <FaLinkedinIn />
+                </Link>
+              </li>
+            </ul>
+          </section>
+        </div>
       </div>
     </main>
   )
