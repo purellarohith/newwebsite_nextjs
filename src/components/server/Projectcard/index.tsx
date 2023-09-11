90daea4cebb857bd45de0583d0d889ac963e0167
rohith purella <rohith.purella@bighaat.com>
2023-09-11 15:58:36 +0530

    error resolved

--- src/components/server/Projectcard/index.tsx
+++ src/components/server/Projectcard/index.tsx
@@ -5,18 +5,25 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from './styles.module.scss'
 
-function ProjectCards(props) {
+interface Props {
+    imgPath: string
+    title: string
+    description: string
+    link: string
+}
+
+function ProjectCards({ imgPath, title, description, link }: Props) {
     return (
         <section className={styles['project-card-view']}>
-            <Image className="mx-auto rounded w-auto max-h-80" src={props.imgPath} width={420} height={320} alt="card-img" />
+            <Image className="mx-auto rounded w-auto max-h-80" src={imgPath} width={420} height={320} alt="card-img" />
             <div className="flex flex-col my-4 justify-between  h-full">
                 <div className="flex flex-col">
-                    <label className="text-white text-lg text-center">{props.title}</label>
+                    <label className="text-white text-lg text-center">{title}</label>
                     <p style={{ textAlign: "justify" }} className="text-white text-base font-medium">
-                        {props.description}
+                        {description}
                     </p>
                 </div>
-                <Link className="bg-primary flex items-center justify-center mx-auto px-4 py-2 rounded-md mt-4" href={props.link} target="_blank">
+                <Link className="bg-primary flex items-center justify-center mx-auto px-4 py-2 rounded-md mt-4" href={link} target="_blank">
                     <CgWebsite /> &nbsp;
                     {"Website"}
                 </Link>
